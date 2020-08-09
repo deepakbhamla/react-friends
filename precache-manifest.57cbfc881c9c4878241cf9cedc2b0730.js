bc0e8c790b2e4c0e8f7fcf54924890c6b60bcb4a
Deepak Bhamla <dbhamla7@gmail.com>
2020-08-09 13:36:28 +0530

    Updates

---
+++ precache-manifest.57cbfc881c9c4878241cf9cedc2b0730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248caf0d9b342821c45b5d55bd211cd3",
+    "url": "/react-friends/index.html"
+  },
+  {
+    "revision": "d6d56eb291e4d98bde72",
+    "url": "/react-friends/static/css/main.cf50721d.chunk.css"
+  },
+  {
+    "revision": "dadd5e5bc1621b3aa903",
+    "url": "/react-friends/static/js/2.d6620123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-friends/static/js/2.d6620123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d56eb291e4d98bde72",
+    "url": "/react-friends/static/js/main.30f3ea93.chunk.js"
+  },
+  {
+    "revision": "b80bb48d74295dbc73fa",
+    "url": "/react-friends/static/js/runtime-main.ebb7b3b6.js"
+  }
+]);
